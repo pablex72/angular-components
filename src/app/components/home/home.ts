7c04fef0d6b8a3c4d485a64f184cdf1c29168616
pablo <tonyp2hoy@gmail.com>
2025-10-20 22:41:59 -0400

    adicionando component principal-home-housingLocation

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HousingLocation } from "../housing-location/housing-location";
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
