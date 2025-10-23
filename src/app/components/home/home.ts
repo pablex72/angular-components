10f460657d54a875cc5c16083b79f6d525d95f35
pablo <tonyp2hoy@gmail.com>
2025-10-22 23:48:43 -0400

    adicionando inputs de housing location

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { HousingLocation } from "../housing-location/housing-location";
+import { HousingLocation } from '../housing-location/housing-location';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
+  readonly baseUrl = 'https://cdn.pixabay.com/photo/2017/03/27/13/28/man-2178721_1280.jpg';
 
+  housinglocation: Housinglocation = {
+    id: 555,
+    name: 'hello',
+    city: 'cbba',
+    state: 'cbba',
+    photo: `${this.baseUrl}`,
+    availableUnits: 44,
+    wifi: true,
+    laundry: true,
+  };
 }
--- src/app/components/housing-location/housing-location.ts
+++ src/app/components/housing-location/housing-location.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   templateUrl: './housing-location.html',
-  styleUrl: './housing-location.css'
+  styleUrl: './housing-location.css',
 })
 export class HousingLocation {
 
+  @Input() housingLocation! : HousingLocation;
+  
 }
